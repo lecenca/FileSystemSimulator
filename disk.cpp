7b85a28a8e466c71e6fc5b868baf7dcdbf9fa6b9
lecenca <isbnmiku@gmail.com>
2017-09-22 20:24:31 +0800

    fix some

--- disk.cpp
+++ disk.cpp
@@ -12,9 +12,9 @@ Disk::~Disk(){
     diskFile.close();
 }
 
-Block Disk::readBlock(uint8_t index){
-    //此处应检查index是否小于127
-    //使用异常？静态断言？或其它？
+Option<Block> Disk::readBlock(uint8_t index){
+    if(index>=128)
+        return Option<Block>();
     uint8_t block[64];
     diskFile.seekg(index*64,std::ios::beg);
     diskFile.read(reinterpret_cast<char*>(block),64);
@@ -22,11 +22,14 @@ Block Disk::readBlock(uint8_t index){
     for(unsigned i = 0;i<64;++i){
         blk[i] = block[i];
     }
-    return blk;
+    return Option<Block>(blk);
 }
 
-void Disk::writeBlock(Block block, uint8_t index){
+bool Disk::writeBlock(Block block, uint8_t index){
+    if(index>=128)
+        return false;
     diskFile.seekp(index*64,std::ios::beg);
     diskFile.write((char*)block.data(),64);
     diskFile.flush();
+    return true;
 }
